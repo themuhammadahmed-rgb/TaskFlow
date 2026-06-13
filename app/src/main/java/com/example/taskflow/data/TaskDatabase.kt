820069dcb814d9b7efd7805ddfad2818f0965b6d
Muhammad Ahmed <muhammad.ahmed250506@gmail.com>
2026-06-13 19:47:13 +0500

    database added

--- app/src/main/java/com/example/taskflow/data/TaskDatabase.kt
+++ app/src/main/java/com/example/taskflow/data/TaskDatabase.kt
@@ -1,14 +1,30 @@
-package com.example.taskflow.data
+package com.example.taskflow.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.taskflow.data.entity.TaskEntity
+import android.content.Context
+import androidx.room.Room
+import com.example.taskflow.data.TaskDao
 
 @Database(entities = [TaskEntity::class], version = 1)
 abstract class TaskDatabase : RoomDatabase() {
     abstract fun taskDao(): TaskDao
 
     companion object {
+        @Volatile
+        private var INSTANCE: TaskDatabase? = null
 
+        fun getDatabase(context: Context): TaskDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    TaskDatabase::class.java,
+                    "task-database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
     }
 }

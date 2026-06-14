741278d58905d029f25289149c8831ee9bef1f9d
Muhammad Ahmed <muhammad.ahmed250506@gmail.com>
2026-06-14 14:46:43 +0500

    database checked & repository file added

--- app/src/main/java/com/example/taskflow/data/TaskDatabase.kt
+++ app/src/main/java/com/example/taskflow/data/TaskDatabase.kt
@@ -6,7 +6,6 @@ import com.example.taskflow.data.entity.TaskEntity
 import android.content.Context
 import androidx.room.Room
 import com.example.taskflow.data.TaskDao
-
 @Database(entities = [TaskEntity::class], version = 1)
 abstract class TaskDatabase : RoomDatabase() {
     abstract fun taskDao(): TaskDao
@@ -14,17 +13,18 @@ abstract class TaskDatabase : RoomDatabase() {
     companion object {
         @Volatile
         private var INSTANCE: TaskDatabase? = null
-
         fun getDatabase(context: Context): TaskDatabase {
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     TaskDatabase::class.java,
                     "task-database"
-                ).build()
+                ).fallbackToDestructiveMigration()
+                    .build()
                 INSTANCE = instance
                 instance
             }
         }
+
     }
 }

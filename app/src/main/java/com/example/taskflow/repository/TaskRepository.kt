6216fc703caa117bfe594fc927cadc515cf9bf94
Muhammad Ahmed <muhammad.ahmed250506@gmail.com>
2026-06-14 15:13:30 +0500

    Complete Data Layer Added

--- app/src/main/java/com/example/taskflow/repository/TaskRepository.kt
+++ app/src/main/java/com/example/taskflow/repository/TaskRepository.kt
@@ -4,6 +4,7 @@ import com.example.taskflow.data.TaskDao
 import com.example.taskflow.data.entity.TaskEntity
 
 class TaskRepository(private val taskDao: TaskDao) {
+
     suspend fun insertTask(task: TaskEntity) {
         taskDao.insert(task)
     }
@@ -21,5 +22,4 @@ class TaskRepository(private val taskDao: TaskDao) {
     fun searchTasks(query: String) = taskDao.searchTasks(query)
 
     suspend fun getTaskById(id: Int) = taskDao.getTaskById(id)
-
 }

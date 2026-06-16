b2b9943992b00abc007754bd4f3b425e2ca1fc67
Muhammad Ahmed <muhammad.ahmed250506@gmail.com>
2026-06-16 22:53:09 +0500

    functions added in TaskViewModel.kt

--- app/src/main/java/com/example/taskflow/viewmodel/TaskViewModel.kt
+++ app/src/main/java/com/example/taskflow/viewmodel/TaskViewModel.kt
@@ -9,16 +9,38 @@ import com.example.taskflow.repository.TaskRepository
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 class TaskViewModel(application: Application) : AndroidViewModel(application) {
     private val database = TaskDatabase.getDatabase(application)
     private val repository = TaskRepository(database.taskDao())
 
 
-    val allTasks: StateFlow<List<TaskEntity>> = repository.getAllTasks()
-        .stateIn(
-            viewModelScope,
-            SharingStarted.WhileSubscribed(5000),
-            emptyList()
+    val allTasks: StateFlow<List<TaskEntity>> = repository.getAllTasks().stateIn(
+            viewModelScope, SharingStarted.WhileSubscribed(5000), emptyList()
         )
+
+    fun addTask(task: TaskEntity) {
+        viewModelScope.launch {
+            repository.insertTask(task)
+        }
+    }
+
+    fun updateTask(task: TaskEntity) {
+        viewModelScope.launch {
+            repository.updateTask(task)
+        }
+    }
+
+    fun deleteTask(task: TaskEntity) {
+        viewModelScope.launch {
+            repository.deleteTask(task)
+        }
+    }
+
+    fun getTaskById(id: Int) {
+        viewModelScope.launch {
+            repository.getTaskById(id)
+        }
+    }
 }
